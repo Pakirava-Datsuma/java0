a90e0aad511bf155312cacbb857dc29064d03201
Dmitriy Kuleshov <pakirava.datsuma@gmail.com>
2016-08-24 15:18:47 +0300

    link lists of books and theirs data

--- src/practice_8/Library.java
+++ src/practice_8/Library.java
@@ -1,19 +1,47 @@
 package practice_8;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by swanta on 13.08.16.
  */
 public class Library {
-    private ObservableList<Book> books = FXCollections.observableArrayList();
-    private List<Book> bookList = books;
+    private final ObservableList<Book> books = FXCollections.observableArrayList();
+    private final List<BookData> booksData = new ArrayList<>();
+    private final ListChangeListener booksDataUpdater = new ListChangeListener<Book>() {
+        {
+            books.addListener(this);
+        }
+
+        @Override
+        public void onChanged(Change<? extends Book> changeList) {
+            while (changeList.next()) {
+                if (changeList.wasAdded()) {
+                    booksData.addAll(
+                            changeList.getAddedSubList().stream()
+                                    .map(Book::getData)
+                                    .collect(Collectors.toList()));
+                } else if (changeList.wasRemoved()) {
+                    booksData.removeAll(
+                            changeList.getRemoved().stream()
+                                    .map(Book::getData)
+                                    .collect(Collectors.toList())
+                    );
+                } else {
+                    throw new RuntimeException("unexpected changes in ObservableList of books in library");
+                }
+            }
+        }
+    };
+
 
     private File libraryFile = new File("testLibrary.lib");
     private boolean isFileLoaded = false;
@@ -27,28 +55,26 @@ public class Library {
     public boolean loadBooksFromFile (){
         isFileLoaded = false;
         try (FileInputStream fis = new FileInputStream(libraryFile)) {
-            Collection<BookData> bookData = new ArrayList();
+            List<BookData> bookData;// = new ArrayList<>();
             ObjectInputStream ois = new ObjectInputStream(fis);
-            bookData = (ArrayList) ois.readObject();
+            bookData = (List<BookData>) ois.readObject();
             this.books.setAll(Book.getBooks(bookData));
             isFileLoaded = true;
+        }  catch (ClassNotFoundException e) {
+            //"no books were saved earlier"
+            e.printStackTrace();
         } catch (IOException e) {
             //"can't access file"
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            //"no books were saved earlier"
             e.printStackTrace();
         }
         return isFileLoaded;
     }
 
     public boolean saveBooksToFile () {
-//        TODO: overwrite exists data
         isFileLoaded = false;
-        try (FileOutputStream fis = new FileOutputStream(libraryFile)) {
+        try (FileOutputStream fis = new FileOutputStream(libraryFile, false)) { //file will be overwritten
             ObjectOutputStream ois = new ObjectOutputStream(fis);
-            Collection<BookData> bookData = Book.getBooksData(books);
-            ois.writeObject(bookData);
+            ois.writeObject(this.booksData);
             isFileLoaded = true;
         } catch (IOException e) {
             e.printStackTrace();
